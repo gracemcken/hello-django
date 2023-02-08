9e739fadf353903877efbaafd6651678eaea6949
gracemcken <grace.ellen.mckenna@gmail.com>
2023-02-08 20:25:40 +0000

    Removed '/' in allowed hosts

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["gracemcken-todo-app.herokuapp.com/"]
+ALLOWED_HOSTS = ["gracemcken-todo-app.herokuapp.com"]
 
 
 # Application definition
